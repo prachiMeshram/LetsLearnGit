33e03981a9fee14b68e9fd404c693ea2b0f6fe05
Prachi Meshram <prachimesh89@gmail.com>
2021-09-28 19:59:31 +0530

    initial commit

---
+++ diff.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    cin>>a>>b;
+
+    int diff = a-b;
+
+    cout<<"The Difference is "<<diff<<endl;
+
+    return 0;
+}
