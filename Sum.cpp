eeea7d60a9ad2a7b05d70eaf994edc9c66f3ec42
Prachi Meshram <prachimesh89@gmail.com>
2021-09-28 19:49:32 +0530

    initial commit

---
+++ Sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    cin>>a>>b;
+
+    int sum = a+b;
+
+    cout<<sum<<endl;
+
+    return 0;
+}
