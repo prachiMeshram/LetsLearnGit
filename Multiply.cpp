2efd37feacbc0f78eed18217495be6146dd3ea5a
Prachi Meshram <prachimesh89@gmail.com>
2021-09-28 23:14:38 +0530

    added Multiply.cpp

---
+++ Multiply.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    cin>>a>>b;
+
+    int multiply = a*b;
+
+    cout<<"The Sum is "<<multiply<<endl;
+
+    return 0;
+}
